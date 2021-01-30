4599ae00c45bcd64efdd8288a3e4bff977a5e10b
unknown <ilef.annabi@gmail.com>
2021-01-30 01:39:51 +0100

    projectweb

---
+++ src/app/formateurs/formateurs.ts
@@ -0,0 +1,28 @@
+import { Formateur } from './formateur';
+export const FORMATEURSITEMS: Formateur[] = [
+{
+    id: 1,
+    name: 'Eya',
+    lastname: 'Ghachem',
+    adress: "Aaouina - ariana ",
+    domaine: "Informatique",
+    email: 'ghachemeya@gmail.com'
+},
+{
+    id: 1,
+    name: 'Amira',
+    lastname: 'Ghachem',
+    adress: "Aaouina - ariana ",
+    domaine: "Informatique",
+    email: 'Amira@gmail.com'
+},
+{
+    id: 1,
+    name: 'Kawther',
+    lastname: 'Ghachem',
+    adress: "Aaouina - ariana ",
+    domaine: "Informatique",
+    email: 'Kawther@gmail.com'
+},
+
+];
---
+++ src/app/participants/participants.ts
@@ -0,0 +1,31 @@
+import { Participant } from './participant';
+export const PARTICIPANTSITEMS: Participant[] = [
+{
+    id: 1,
+    name: 'Eya',
+    lastname: 'Ghachem',
+    adress: "aaouina - ariana",
+    societe: "Informatique",
+    poste : "developper mobile ",
+    email: 'ghachem@informatique.com'
+},
+{
+    id: 2,
+    name: 'Khalil',
+    lastname: 'Jendoubi',
+    adress: "ghazala",
+    societe: "informatique",
+    poste : "developper",
+    email: 'Khalil@informatique.fr'
+},
+{
+    id: 3,
+    name: 'Farook',
+    lastname: 'Djaziri',
+    adress: "Monastir",
+    societe: "informatique",
+    poste : "developper",
+    email: 'Farook@informatique.fr'
+},
+
+];
